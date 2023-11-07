a6eabc2a30e14b38ec6cced9424cf4b70ba232f2
jsveleda <jsveleda@gmail.com>
2023-11-06 23:57:30 -0300

    _

---
+++ app/virtual_users.py
@@ -0,0 +1,17 @@
+import random
+from locust import HttpUser, task, between
+
+class VirtualUser(HttpUser):
+    wait_time = between(1, 5)  # Wait between 1 to 5 seconds between tasks
+
+    @task(25)
+    def access_read_route(self):
+        key = f'key_{random.randint(0, 9999)}' 
+        self.client.get(f"/read/{key}")
+
+    @task(75)
+    def acess_create_route(self):
+        key = f'key_{random.randint(0, 9999)}' 
+        value = str(random.randint(0, 9999))
+        self.client.post(f"/create/{key}/{value}")
+
